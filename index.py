a341263e7c7b1034e9ba5846db5d700b11d62a56
Fernando <fernandoarroyo0011@gmail.com>
2022-02-16 22:02:13 -0400

    Subiendo mis ejercicios

---
+++ index.py
@@ -0,0 +1,17 @@
+print('Hola Python');
+
+#Creando mi primer hola mundo en python
+#Tucumana
+
+
+saludo = '|Hola mundo|'
+
+print(saludo)
+
+#Mejorando un poco
+
+nombre = input("Escriba su nombre")
+print("Hola ", nombre)
+
+
+print('Comiteando los cambios con git -a -m')
