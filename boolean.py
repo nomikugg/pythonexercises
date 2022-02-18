3049163310180c3f2938b023b3236d846248a0f6
Fernando <fernandoarroyo0011@gmail.com>
2022-02-17 23:46:11 -0400

    Boolean

---
+++ boolean.py
@@ -0,0 +1,22 @@
+#Boolean value
+
+
+
+
+vall = True
+print(vall) #True
+
+#Number to Boolean
+number = 10
+print(bool(number)) #True
+
+number = -5
+print(bool(number)) #True
+
+number = 0
+print(bool(number)) # False
+
+#Boolean from comparison operator
+val1 = 6
+val2 = 7
+print(val1<val2)
